9d1334b817a212991ae7ba3ff0dcac8e016d54e3
ppajda <pajdek@gmail.com>
2017-08-23 10:03:05 +0200

    updates

--- device.mk
+++ device.mk
@@ -67,6 +67,10 @@ PRODUCT_PACKAGES += \
     FMRadio \
     libfmjni
 
+# Glove mode
+PRODUCT_PACKAGES += \
+    GloveMode
+
 PRODUCT_COPY_FILES += \
     device/lge/g3-common/wcnss/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     device/lge/g3-common/wcnss/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
